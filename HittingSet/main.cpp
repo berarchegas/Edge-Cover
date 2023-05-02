59f7e97920550526bf087e9352ddb74eb0706f76
berarchegas <b246970@dac.unicamp.br>
2023-05-02 16:34:09 -0300

    Comecando o bb

--- HittingSet/main.cpp
+++ HittingSet/main.cpp
@@ -17,15 +17,18 @@ using pii = pair<int, int>;
 
 // Do we create a new Set Trie every time or do we create an erase operation ? 
 
+// Order of operations
+
 // Design Decision: We are maintaining a few global structures to avoid memory redundancy
 // We need to be able to reset these structures properly before using each one
 // We could also test always going through every vertex and edge instead of getting vectors with active elements
 
+int upperBound = 1000000;
 
-bool valid[10];
+bool *valid;
 
 // We need to reset maxLowerBound properly 
-int n, m, maxDegree[10][2], maxDegreeNode[10][2], maxLowerBound[10];
+int n, m, **maxDegree, **maxDegreeNode, *maxLowerBound;
 vector<vector<int>> bucket;
 
 // blockedEdges by each vertex in the costly discard packing bound
@@ -33,8 +36,108 @@ vector<vector<int>> blockedEdges;
 
 // edges[i] are the vertices of the i-th edge
 // vertices[i] are the edges of the i-th vertex
-OrderedSubsetList edges[10], vertices[10], validVertices, validEdges;
+OrderedSubsetList *edges, *vertices, validVertices, validEdges;
 stack<pii> operations;
+
+
+// if we decide something because of the bounds we dont have to branch duh
+int branchAndBound(int shift) {
+
+    if (validEdges.getSize() == 0) 
+        return shift;
+
+    int initialSize = operations.size();
+
+    upperBound = min(upperBound, calculateUpperbound(m, vertices, edges, 
+        valid, bucket, validVertices, validEdges, operations) + shift);
+
+    int lowerBound = 0;
+
+    lowerBound = max(lowerBound, maxDegreeBound(vertices, validVertices, validEdges) + shift);
+
+    if (lowerBound > upperBound) return upperBound;
+
+    lowerBound = max(lowerBound, efficiencyBound(vertices, edges, validEdges, 
+        validVertices, maxDegree, maxDegreeNode, maxLowerBound) + shift);
+
+    if (lowerBound > upperBound) return upperBound;
+    
+    lowerBound = max(lowerBound, packingBound(edges, validEdges, valid) + shift);
+
+    if (lowerBound > upperBound) return upperBound;
+    
+    lowerBound = max(lowerBound, sumOverPackingBound(vertices, 
+        edges, validVertices, validEdges, valid) + shift);
+
+    if (lowerBound > upperBound) return upperBound;
+
+    vector<int> unitEdges = unitEdgeRule(edges, validEdges);
+
+    for (int x : unitEdges) {
+        if (edges[x].getSize() == 1) {
+            int vertice = edges[x].elements()[0];
+
+            int beforeSize = operations.size();
+            takeVertex(vertice, vertices, edges, valid, validVertices, validEdges, operations);
+            upperBound = max(upperBound, branchAndBound(shift + 1));
+            while (operations.size() > beforeSize) 
+                undo(vertices, edges, valid, validVertices, validEdges, operations, bucket);
+
+        }
+        else {
+            eraseEdge(x, vertices, edges, validEdges, operations);
+        }
+    }
+
+    vector<int> nodes = validVertices.elements();
+
+    for (int x : nodes) {
+        if (maxLowerBound[x] > upperBound) {
+
+            int beforeSize = operations.size();
+            takeVertex(x, vertices, edges, valid, validVertices, validEdges, operations);
+            upperBound = max(upperBound, branchAndBound(shift + 1));
+            while (operations.size() > beforeSize) 
+                undo(vertices, edges, valid, validVertices, validEdges, operations, bucket);
+
+        }
+    }
+
+    costlyDiscardPackingBound(edges, validEdges, validVertices, valid, blockedEdges, maxLowerBound);
+
+    repacking(vertices, edges, validVertices, validEdges, valid, operations, maxLowerBound, bucket);
+
+    for (int x : nodes) {
+        if (maxLowerBound[x] > upperBound) {
+
+            int beforeSize = operations.size();
+            takeVertex(x, vertices, edges, valid, validVertices, validEdges, operations);
+            upperBound = max(upperBound, branchAndBound(shift + 1));
+            while (operations.size() > beforeSize) 
+                undo(vertices, edges, valid, validVertices, validEdges, operations, bucket);
+
+        }
+    }
+
+    vector<int> invalidEdges = dominatedEdges(edges, validEdges);
+
+    for (int x : invalidEdges) {
+        eraseEdge(x, vertices, edges, validEdges, operations);
+    }
+
+    vector<int> invalidVertices = dominatedVertices(vertices, validVertices);
+
+    for (int x : invalidVertices) {
+        int beforeSize = operations.size();
+        ignoreVertex(x, vertices, edges, valid, validVertices, operations);
+        upperBound = max(upperBound, branchAndBound(shift));
+        while (operations.size() > beforeSize) 
+            undo(vertices, edges, valid, validVertices, validEdges, operations, bucket);
+    }
+
+    while (operations.size() > initialSize) 
+        undo(vertices, edges, valid, validVertices, validEdges, operations, bucket);
+}
 
 int main() {
     cin >> n >> m;
@@ -66,35 +169,7 @@ int main() {
         bucket[vertices[i].getSize()].push_back(i);
     }
 
-    printAll(n, m, vertices, edges);
-
-    cout << calculateUpperbound(m, vertices, edges, valid, bucket, validVertices, validEdges, operations) << '\n';
-
-    eraseEdge(3, vertices, edges, validEdges, operations);
-
-    ignoreVertex(0, vertices, edges, valid, validVertices, operations);
-
-    calculateUpperbound(m, vertices, edges, valid, bucket, validVertices, validEdges, operations);
-
-    takeVertex(2, vertices, edges, valid, validVertices, validEdges, operations);
-
-    eraseEdge(2, vertices, edges, validEdges, operations);
-
-    calculateUpperbound(m, vertices, edges, valid, bucket, validVertices, validEdges, operations);
-
-    eraseEdge(1, vertices, edges, validEdges, operations);
-
-    ignoreVertex(1, vertices, edges, valid, validVertices, operations);
-
-    calculateUpperbound(m, vertices, edges, valid, bucket, validVertices, validEdges, operations);
-
-    takeVertex(4, vertices, edges, valid, validVertices, validEdges, operations);
-
-    while (!operations.empty()) undo(vertices, edges, valid, validVertices, validEdges, operations, bucket);
-
-    cout << calculateUpperbound(m, vertices, edges, valid, bucket, validVertices, validEdges, operations) << '\n';
-
-    printAll(n, m, vertices, edges);
+    cout << branchAndBound(0) << '\n';
 
     return 0;
 }
